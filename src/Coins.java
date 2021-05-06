a973caaf8898d268cb39c43aaf43857caa1eec9b
MariaCFFrandsen <56017845+MariaCFFrandsen@users.noreply.github.com>
2021-05-06 11:31:03 +0200

    done

--- src/Coins.java
+++ src/Coins.java
@@ -3,30 +3,36 @@ import java.util.HashMap;
 public class Coins
 {
 
-  private HashMap<Integer, Integer> map = new HashMap<>();
-
-
+  private static HashMap<Integer, Integer>  map = new HashMap<>();
 
   static int minCoins(int coins[], int amount)
   {
     if (amount == 0) return 0;
 
-    // Initialize result
     int res = amount;
+    int sub_res = 0;
 
-    // Try every coin that has smaller value than V
     for (int i=0; i< coins.length; i++)
     {
       if (coins[i] <= amount)
       {
-        int sub_res = minCoins(coins, amount-coins[i]);
-
-        // Check for INT_MAX to avoid overflow and see if
-        // result can minimized
+        if (map.containsValue(amount-coins[i]))
+        {
+          sub_res = map.get(amount-coins[i]);
+        }
+        else
+        {
+          sub_res = minCoins(coins, amount-coins[i]);
+          map.put(amount-coins[i], sub_res);
+        }
         if (sub_res + 1 < res)
+        {
           res = sub_res + 1;
+        }
+
       }
     }
+    map.put(amount, res);
     return res;
   }
 }
--- src/main.java
+++ src/main.java
@@ -3,8 +3,11 @@ public class main
   public static void main(String[] args)
   {
     int coins[] = {22, 10, 7, 1};
-    int V = 105;
+    int V = 80;
     System.out.println ( "Minimum coins required is "
+        + Coins.minCoins(coins, V));
+
+    System.out.println("Minimum coins required is "
         + Coins.minCoins(coins, V));
   }
 }
